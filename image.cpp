1586da26cf045d6f47a0cf16375d29e3b0dfeb71
S <Shivaraja.S@philips.com>
2024-11-18 21:12:36 +0530

    Move brightener() to Image class and fix brightened image access issue

---
+++ image.cpp
@@ -0,0 +1,26 @@
+#include"image.h"
+
+
+Image::Image(uint16_t rows, uint16_t columns) : m_rows(rows), m_columns(columns) {
+    pixels = std::make_unique<uint8_t[]>(rows * columns);
+}
+
+Image::~Image() {
+    std::cout << "Image destroyed. Pixel memory automatically freed.\n";
+}
+
+uint8_t Image::GetPixel(uint16_t x, uint16_t y) const {
+    return pixels[x * m_columns + y];
+}
+
+void Image::SetPixel(uint16_t x, uint16_t y, uint8_t value) {
+    pixels[x * m_columns + y] = value;
+}
+bool Image::ValidateImage() const{
+    if (m_columns <= 1024 && m_rows <= 1024) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- image.h
+++ image.h
@@ -5,19 +5,13 @@
 
 class Image {
  public:
-    Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
-        pixels = std::make_unique<uint8_t[]>(rows * columns);
-    }
-    ~Image() {
-        std::cout << "Image destroyed. Pixel memory automatically freed.\n";
-    }
-    uint8_t GetPixel(uint16_t x, uint16_t y) const {
-        return pixels[x * m_columns + y];
-    }
-    void SetPixel(uint16_t x, uint16_t y, uint8_t value) {
-        pixels[x * m_columns + y] = value;
-    }
-    const uint16_t m_rows;
-    const uint16_t m_columns;
-    std::unique_ptr<uint8_t[]> pixels;  // max 1k x 1k image
+     Image(uint16_t rows, uint16_t columns);
+     ~Image();
+     uint8_t GetPixel(uint16_t x, uint16_t y) const;
+     void SetPixel(uint16_t x, uint16_t y, uint8_t value);
+     bool ValidateImage() const;
+     
+     const uint16_t m_rows;
+     const uint16_t m_columns;
+     std::unique_ptr<uint8_t[]> pixels;  // max 1k x 1k image
 };
